0bf5b6a2d7516f818cb175527362d61b3cc3cbed
kannanATexcelforte <kannan@excelforte.com>
2023-08-30 11:43:30 +0530

    30.08.2023-etas
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `<p>Hello World</p>` 
+  // template: `<p>Hello World</p>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css'],
+  // styles: ['']
 })
 
 export class AppComponent {
