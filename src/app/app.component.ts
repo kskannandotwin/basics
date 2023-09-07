9124cb20cfbe6928fc37f20e01f19f77d5ab1490
kannanATexcelforte <kannan@excelforte.com>
2023-09-07 12:14:20 +0530

    07.09.2023-ee
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,8 @@ export class AppComponent {
   changeImage(e: KeyboardEvent) {
     this.imgUrl = (e.target as HTMLInputElement).value;
   }
+
+  logImg(event: string) {
+    console.log(event);
+  }
 }
