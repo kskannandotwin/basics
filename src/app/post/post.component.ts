c7425ce02cc2ae0722facb814c7f9baa222ae43f
kannanATexcelforte <kannan@excelforte.com>
2023-10-04 11:39:25 +0530

    04.10.2023-dlh
    completed

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input('img') postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('constructor() called', this.postImg);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit() called', this.postImg);
+  }
 }
