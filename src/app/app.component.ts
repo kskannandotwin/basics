9009cbdfd8f9b05565b4fd07f5f0ad141161fb4d
kannanATexcelforte <kannan@excelforte.com>
2023-10-06 12:38:13 +0530

    06.10.2023-nd[directive]
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,11 @@ import { Component } from "@angular/core";
 export class AppComponent {
   name = 'luis ramirez';
   imgUrl = 'https://picsum.photos/id/237/500/500';
+  images = [
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500'
+  ];
   currentDate = new Date();
   cost = 2000;
   temperature = 25.3;
